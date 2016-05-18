22bf613d300ad15f4d62555e6a8756350bf624a0
James Dabbs <jamesdabbs@gmail.com>
2016-05-18 10:38:13 -0400

    Add word count

--- week-1/3/crypto.rb
+++ week-1/3/crypto.rb
@@ -1,11 +1,21 @@
+require "pry"
+
 text = "the quick brown fox jumps things"
 
-def letter_count string
+def letter_count(string)
   string.length
 end
 
-letters = letter_count text
-puts "We had #{letters} letters"
+# letters = letter_count(text)
+# puts "We had #{letters} letters"
+#
+# letters = letter_count("this is inline")
+# puts "And #{letters} letters"
 
-letters = letter_count "this is inline"
-puts "And #{letters} letters"
+def word_count corpus
+  corpus.split.count
+end
+
+words = word_count text
+puts "We had #{words} words"
+puts "Or #{word_count text} words"

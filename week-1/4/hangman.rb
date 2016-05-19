b43b638e635351a1c22f9cf5585b59bf7037e2e3
James Dabbs <jamesdabbs@gmail.com>
2016-05-19 09:31:17 -0400

    Pick word and show board

--- week-1/4/hangman.rb
+++ week-1/4/hangman.rb
@@ -11,6 +11,27 @@ def player_wants_to_quit?
   end
 end
 
+def game_over?
+  # out_of_turns || won?
+  false
+end
+
+def get_a_word
+  [
+    "aardwolf",
+    "banana",
+    "rails",
+    "fizzbuzz"
+  ].sample
+end
+
+def display_board
+  word = get_a_word
+  warn "The word is: #{word}"
+  board = ["_"] * word.length
+  puts "The board is: #{board.join(' ')}"
+end
+
 loop do
   # Play game one time
   until game_over?

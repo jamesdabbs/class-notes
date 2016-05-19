518b4294cd6d89cef6fdb845eeeb75cbf6dba228
James Dabbs <jamesdabbs@gmail.com>
2016-05-19 09:22:17 -0400

    Add quit functionality

--- week-1/4/hangman.rb
+++ week-1/4/hangman.rb
@@ -1,10 +1,22 @@
 require "pry"
 
-until player_wants_to_quit
+def player_wants_to_quit?
+  puts "Do you want to keep playing (y/n)?"
+  input = gets.chomp
+  # input == "n"
+  if input == "n"
+    true
+  else
+    false
+  end
+end
+
+loop do
   # Play game one time
   until game_over?
     display_board
     choose_letter
     record_move
   end
+  break if player_wants_to_quit?
 end

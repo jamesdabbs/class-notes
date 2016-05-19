49277d2374956bdc75341c1cf175b81c8e3b544d
James Dabbs <jamesdabbs@gmail.com>
2016-05-19 11:31:18 -0400

    Extract letter helper function

--- week-1/4/words_words_words.rb
+++ week-1/4/words_words_words.rb
@@ -1,17 +1,28 @@
 require "pry"
 
-def num_words_starting_with_t text
+def num_words_starting_with_letter text, letter
   total = 0
-  words = text.split(" ")
+  words = text.split " "
   words.each do |w|
-    if w[0] == "t" || w[0] == "T"
+    if w[0].downcase == letter.downcase
       total += 1
     end
   end
   total
 end
 
+def num_words_starting_with_t text
+  num_words_starting_with_letter text, "t"
+end
+
+def num_words_starting_with_m text
+  num_words_starting_with_letter text, "m"
+end
+
 str = File.read "moog.txt"
 
 x = num_words_starting_with_t str
 puts "There are #{x} words starting with T"
+
+x = num_words_starting_with_m str
+puts "There are #{x} words starting with M"

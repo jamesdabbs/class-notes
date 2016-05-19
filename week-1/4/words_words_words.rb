0bd39df5244c3f1b0ce273d0ef81a36f15303460
James Dabbs <jamesdabbs@gmail.com>
2016-05-19 11:26:53 -0400

    Add first function

--- week-1/4/words_words_words.rb
+++ week-1/4/words_words_words.rb
@@ -1,7 +1,17 @@
-def words_starting_with_t text
+require "pry"
+
+def num_words_starting_with_t text
+  total = 0
+  words = text.split(" ")
+  words.each do |w|
+    if w[0] == "t" || w[0] == "T"
+      total += 1
+    end
+  end
+  total
 end
 
 str = File.read "moog.txt"
 
-x = words_starting_with_t str
+x = num_words_starting_with_t str
 puts "There are #{x} words starting with T"

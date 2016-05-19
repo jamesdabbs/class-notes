106436a0eeb7e6fc9fbd2aa1eb58876919d2d195
James Dabbs <jamesdabbs@gmail.com>
2016-05-19 11:58:00 -0400

    Refactor using enumerable methods

--- week-1/4/arrays.rb
+++ week-1/4/arrays.rb
@@ -1,20 +1,28 @@
 require "pry"
 
 def sum_of_array numbers
-  sum = 0
-  numbers.each do |n|
-    sum += n.to_i
+  # sum = 0
+  # numbers.each do |n|
+  #   sum += n.to_i
+  # end
+  # sum
+
+  # numbers.map { |n| n.to_i }.reduce { |a,b| a + b }
+  numbers.reduce 0 do |accumulator, n|
+    accumulator + n.to_i
   end
-  sum
 end
 
 def convert_lines_to_row_sums lines
-  row_sums = []
-  lines.each do |line|
-    row_sum = sum_of_array line.split(",")
-    row_sums.push row_sum
+  # row_sums = []
+  # lines.each do |line|
+  #   row_sum = sum_of_array line.split(",")
+  #   row_sums.push row_sum
+  # end
+  # row_sums
+  lines.map do |line|
+    sum_of_array line.split(",")
   end
-  row_sums
 end
 
 def sum_of_all_lines lines
@@ -24,11 +32,14 @@ end
 
 def awkward_row_sum lines
   rows = convert_lines_to_row_sums lines
-  big_rows = []
-  rows.each do |s|
-    if s > 250
-      big_rows.push s
-    end
+  # big_rows = []
+  # rows.each do |s|
+  #   if s > 250
+  #     big_rows.push s
+  #   end
+  # end
+  big_rows = rows.select do |s|
+    s > 250
   end
   sum_of_array big_rows
 end

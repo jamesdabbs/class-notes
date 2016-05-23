29372e9af3c408e0cb9680dbc2517fc57800c634
James Dabbs <jamesdabbs@gmail.com>
2016-05-23 11:06:03 -0400

    Refactor instance variables

--- week-2/item.rb
+++ week-2/item.rb
@@ -4,24 +4,29 @@ class Item
   BaseTax = 1.07
 
   def initialize starting_price
-    @my_price = starting_price
+    @base_price = starting_price
+    @is_on_sale = false
+  end
+
+  def is_on_sale?
+    @is_on_sale
   end
 
   def price
-    if @discount_price
-      @discount_price
+    if is_on_sale?
+      (@base_price * 0.9).round(2)
     else
-      @my_price
+      @base_price
     end
   end
 
   def price_with_tax
-    # (@my_price * BaseTax).round(2)
+    # (@base_price * BaseTax).round(2)
     (price * 1.07).round(2)
   end
 
   def add_discount
-    @discount_price = (@my_price * 0.9).round(2)
+    @is_on_sale = true
   end
   def add_discout
     add_discount

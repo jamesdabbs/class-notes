91a5ba05017f1fa5881ff8c7bc2ee27419528b97
James Dabbs <jamesdabbs@gmail.com>
2016-05-23 10:38:07 -0400

    Add effectful method

--- week-2/item.rb
+++ week-2/item.rb
@@ -1,12 +1,22 @@
 require "pry"
 
 class Item
+  def initialize
+    @my_price = 100
+  end
+
   def price
-    rand(1 .. 100)
+    @my_price
   end
 
   def price_with_tax
-    (price * 1.07).round(2)
+    (@my_price * 1.07).round(2)
+    # (price * 1.07).round(2)
+  end
+
+  def add_discount
+    with_discount = (@my_price * 0.9).round(2)
+    @my_price = with_discount
   end
 end
 

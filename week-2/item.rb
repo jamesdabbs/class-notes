4dbe4b511b5ac9f08acfb78c9b9daf929abdf103
James Dabbs <jamesdabbs@gmail.com>
2016-05-23 10:55:59 -0400

    Working discount update

--- week-2/item.rb
+++ week-2/item.rb
@@ -8,17 +8,20 @@ class Item
   end
 
   def price
-    @my_price
+    if @discount_price
+      @discount_price
+    else
+      @my_price
+    end
   end
 
   def price_with_tax
-    (@my_price * BaseTax).round(2)
-    # (price * 1.07).round(2)
+    # (@my_price * BaseTax).round(2)
+    (price * 1.07).round(2)
   end
 
   def add_discount
-    with_discount = (@my_price * 0.9).round(2)
-    @my_price = with_discount
+    @discount_price = (@my_price * 0.9).round(2)
   end
   def add_discout
     add_discount

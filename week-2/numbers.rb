b3bf26f902a4422ca889cc715ff90b285ba0db3c
James Dabbs <jamesdabbs@gmail.com>
2016-05-23 11:29:12 -0400

    Pass data through methods

--- week-2/numbers.rb
+++ week-2/numbers.rb
@@ -12,17 +12,24 @@ class NumberGame
 
   def ask_for_number
     puts "What is your guess?"
-    gets.chomp
+    gets.chomp.to_i
   end
 
-  def check_number
+  def check_number guess
+    if guess == @answer
+      puts "You win!"
+    elsif guess > @answer
+      puts "Too high"
+    else
+      puts "Too low"
+    end
   end
 end
 
 g = NumberGame.new
 until g.over?
-  g.ask_for_number
-  g.check_number
+  n = g.ask_for_number
+  g.check_number n
 end
 
 binding.pry

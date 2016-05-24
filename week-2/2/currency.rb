ed40cc267e35a177d38d73fc20373970f7d15b11
James Dabbs <jamesdabbs@gmail.com>
2016-05-24 09:30:40 -0400

    Add times method

--- week-2/2/currency.rb
+++ week-2/2/currency.rb
@@ -1,8 +1,15 @@
+require "pry"
+
 class Currency
   attr_reader :amount, :denomination
 
   def initialize amt, denom
     @amount, @denomination = amt, denom
+  end
+
+  def times n
+    new_amount = amount * n
+    Currency.new(new_amount, denomination)
   end
 end
 
@@ -11,3 +18,7 @@ ten_dollars = Currency.new(10, :usd)
 
 puts "#{five_dollars.amount} is 5"
 puts "#{five_dollars.denomination} is usd"
+
+prod = five_dollars.times(4)
+puts "prod is #{prod.amount}#{prod.denomination}"
+# binding.pry

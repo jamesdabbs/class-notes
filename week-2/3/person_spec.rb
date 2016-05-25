ce40cc4793a6f9b0d7180d7145db7de3288d034e
James Dabbs <jamesdabbs@gmail.com>
2016-05-25 09:49:19 -0400

    Passing specs

--- week-2/3/person_spec.rb
+++ week-2/3/person_spec.rb
@@ -1,4 +1,6 @@
+require "pry"
 require "minitest/autorun"
+require "minitest/focus"
 
 begin
   require "minitest/reporters"
@@ -8,7 +10,27 @@ rescue LoadError
 end
 
 class Person
-  # Your code goes here
+  def initialize first, last=nil
+    @first = first
+    @last  = last
+  end
+
+  def self.parse string
+    first, last = string.split " "
+    Person.new first, last
+  end
+
+  def first_name
+    @first
+  end
+
+  def last_name
+    @last
+  end
+
+  def name
+    "#{first_name} #{last_name}".strip
+  end
 end
 
 class PersonTests < Minitest::Test
@@ -18,26 +40,22 @@ class PersonTests < Minitest::Test
   end
 
   def test_people_have_last_names
-    skip
     p = Person.new "Russell", "Osborne"
     assert_equal "Osborne", p.last_name
   end
 
   def test_people_have_full_names
-    skip
     p = Person.new "James", "Dabbs"
     assert_equal "James Dabbs", p.name
   end
 
   def test_some_people_dont_have_last_names
-    skip
     p = Person.new "Madonna"
     assert_equal "Madonna", p.name
   end
 
   # Harder!
   def test_you_can_build_a_person_from_a_full_name
-    skip
     p = Person.parse "Dana Calder"
     assert_equal "Dana", p.first_name
   end

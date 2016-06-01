1328a1510f236d599f58abdb4e805f0de4eaabcf
James Dabbs <jamesdabbs@gmail.com>
2016-06-01 09:11:37 -0400

    Add optional argument

--- week-3/2/arguments.rb
+++ week-3/2/arguments.rb
@@ -1,13 +1,23 @@
+require 'pry'
+
 class Parser
-  attr_reader :path
-  
-  def initialize path
-    @path = path
+  attr_reader :path, :string
+
+  def initialize options
+    @path = options[:path]
+    @string = options[:string]
   end
 
   def run
-    puts "Parsing data from path: #{path}"
+    if path
+      puts "Parsing data from path: #{path}"
+      puts File.read path
+    elsif string
+      puts "Using raw string"
+      puts string
+    end
   end
 end
 
-Parser.new(__FILE__).run
+Parser.new(path: __FILE__).run
+Parser.new(string: "Here's a string").run

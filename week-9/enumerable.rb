08a93c55751b0c348098b2dd6ccb3e246edf8c97
James Dabbs <jamesdabbs@gmail.com>
2016-07-12 10:34:30 -0400

    More yield examples

--- week-9/enumerable.rb
+++ week-9/enumerable.rb
@@ -9,29 +9,58 @@ require "pry"
 #   Admin     = Role.new "System administrator", :admin, Float::INFINITY
 # end
 
-class Counter
-  def initialize stop=10
-    @start = 0
-    @stop  = stop
-  end
+# class Counter
+#   def initialize stop=10
+#     @start = 0
+#     @stop  = stop
+#   end
+#
+#   def floop n
+#     puts "n is #{n}"
+#     yield 1
+#     yield 2341234
+#     # sleep 1
+#     yield 345
+#   end
+#
+#   def floop2 thing_to_do
+#     thing_to_do.call "from floop2"
+#   end
+# end
+#
+# c = Counter.new 20
+# c.floop 10 do |n|
+#   i = n.to_s.reverse.to_i
+#   puts "#{i} ~ #{n}"
+# end
+#
+# # yell = Proc.new do |v|
+# #   puts v.upcase
+# # end
+# class Upcaser
+#   def call str
+#     puts str.upcase
+#   end
+# end
+# c.floop2 Upcaser.new
 
-  def each n, &block
-    puts "n is #{n}"
-    # @start.upto(@stop).each do |number|
-    #   block.call number
-    # end
-    block.call 1
-    block.call 2341234
-    sleep 1
-    block.call 345
+class MyObj
+  attr_accessor :name, :email
+
+  def initialize
+    yield self
+    freeze
   end
 end
 
-c = Counter.new 20
-c.each 10 do |n|
-  i = n.to_s.reverse.to_i
-  puts "#{i} ~ #{n}"
+dana = MyObj.new do |o|
+  o.name  = "Dana"
+  o.email = "dana@theironyard.com"
 end
 
-# binding.pry
+File.open "file.txt", "w" do |f|
+  f.puts "something"
+end
+
+binding.pry
 puts
